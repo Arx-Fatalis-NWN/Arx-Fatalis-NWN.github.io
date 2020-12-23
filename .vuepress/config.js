34d44fbca641d504d3f5a7d3599bdf7c35a51859
Saint Wish <SaintWish@users.noreply.github.com>
2020-12-23 15:27:56 -0700

    vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,71 @@
+module.exports = {
+  base: '/',
+  title: 'ArxFatalis NWN',
+  description: 'A RP NWN server based on the Arx Fatalis universe.',
+
+  plugins: [
+    ['@vuepress/search', {
+      searchMaxSuggestions: 10
+    }],
+    ['vuepress-plugin-container', {
+      type: 'warning',
+    }],
+    ['vuepress-plugin-container', {
+      type: 'tip',
+    }],
+    ['vuepress-plugin-container', {
+      type: 'danger',
+    }],
+  ],
+
+  head: [
+    //['link', { rel: 'icon', type: 'image/png', href: '/icon-16x16.png', sizes: '16x16' }],
+    //['link', { rel: 'icon', type: 'image/png', href: '/icon-32x32.png', sizes: '32x32' }],
+    //['link', { rel: 'shortcut icon', href: '/icon.ico' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+  ],
+
+  themeConfig: {
+    repo: 'https://github.com/Arx-Fatalis-NWN',
+    docsRepo: 'Arx-Fatalis-NWN/Arx-Fatalis-NWN.github.io',
+    editLinks: true,
+    editLinkText: 'Help us improve this page.',
+    lastUpdated: true,
+    //logo: '/icon.png',
+    nav: [
+      {
+        text: 'Documentation',
+        link: '/project/intro.md',
+      },
+      {
+        text: 'Discord',
+        link: 'https://discord.gg/cZY8K9n2d9'
+      },
+    ],
+    sidebar: {
+      '/': [
+        {
+          title: 'Information',
+          collapsable: false,
+          children: [
+            '/project/intro.md',
+          ]
+        },
+        {
+          title: 'Guides',
+          collapsable: false,
+          children: [
+          ]
+        },
+        {
+          title: 'In-Game',
+          collapsable: false,
+          children: [
+          ]
+        },
+      ],
+    }
+  },
+}
